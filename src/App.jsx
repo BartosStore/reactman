2e9aebf28f0a8e95e725476df9631a5e4ca695be
Miroslav Bartoš <mirek-b@seznam.cz>
2019-10-01 19:17:03 +0200

    Fetch some data from Github

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,47 @@ import './App.css';
 
 export default class App extends Component {
   state = {
-    name: 'reactman',
-    developer: 'bartosdev',
+    name: 'Reactman',
+    developer: 'Miroslav Bartoš',
+    job: 'Software Developer',
+    githubLogin: '',
+    githubUrl: '',
+    githubReposUrl: '',
+    githubPublicRepos: '',
+    footer: 'This is page footer.',
   };
+
+  componentDidMount() {
+    fetch('https://api.github.com/users/BartosStore')
+      .then(result => result.json()).then((data) => {
+        // console.log(data);
+        this.setState({
+          githubLogin: data.login,
+          githubUrl: data.url,
+          githubReposUrl: data.repos_url,
+          githubPublicRepos: data.public_repos,
+        });
+      });
+  }
 
   render() {
     return (
       <div className="App">
-        <h1>Welcome to {this.state.name}</h1>
-        <span>This is first static text.</span><br />
-        <span>Author is: {this.state.developer}</span>
+        <div className="bar">
+          <div className="logo">MBartos</div>
+          <div className="navigation">Home Articles Contact</div>
+        </div>
+        <div className="cover">
+          <h1>Welcome to {this.state.name} application</h1>
+          <div>{this.state.developer}</div>
+          <div>{this.state.job}</div>
+          <div>{this.state.githubLogin}</div>
+          <div>{this.state.githubUrl}</div>
+          <div>{this.state.githubReposUrl}</div>
+          <div>Number of public repos: {this.state.githubPublicRepos}</div>
+          <div>This is React library playground.</div>
+        </div>
+        <div className="footer">{this.state.footer}</div>
       </div>
     );
   }
